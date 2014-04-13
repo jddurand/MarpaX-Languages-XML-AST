4d52eb39588e1e83624b70e57db81d28d4673f10
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-04-13 23:45:17 +0200

    Include Marpa in my build

--- BuildMarpa.pl
+++ BuildMarpa.pl
@@ -1,21 +1,60 @@
 #!env perl
+use strict;
+use diagnostics;
 use File::Spec;
 use Archive::Extract;
 use Config::AutoConf;        # Just to make sure Config::AutoConf is loaded if Marpa needs it
-use POSIX /EXIT_SUCCESS/;
+use POSIX qw/EXIT_SUCCESS/;
+use File::Find;
+use File::Basename;
+use Config;
+use Cwd;
+use File::Copy qw/move/;
 
-my ($targz, $workdir) = @ARGV;
+my $targz = shift;
+
+print "\n";
+print "*** Extracting $targz\n";
+print "\n";
 
 my $ae = Archive::Extract->new(archive => $targz);
-my $ok = $ae->extract(to => $workdir) || die "Failed to extract $targz, $ae->error";
+my $ok = $ae->extract() || die "Failed to extract $targz, $ae->error";
+my $outdir = $ae->extract_path();
 
-my $olddir = chdir($workdir) || die "Failed to chdir to $workdir, $!";
+print "\n";
+print "*** Building libmarpa in $outdir\n";
+print "\n";
 
-system($^X, 'Build.PL', 'make') || die "Failed to do perl Build.PL make, $?";
+my $olddir = cwd();
+chdir($outdir) || die "Failed to chdir to $outdir, $!";
+
+system($^X, File::Spec->catfile(File::Spec->curdir, 'Build.PL'));
+system(File::Spec->catfile(File::Spec->curdir, 'Build'));
 
 chdir($olddir) || die "Failed to chdir to $olddir, $!";
 
-open(PHONY, '>', 'marpa.PHONY') || die "Failed to open marpa.PHONY, $!";
-close(PHONY) || warn "Failed to close marpa.PHONY, $!";
+my $wanteddir = File::Spec->catdir($outdir, 'libmarpa_build');
+my $quotemetalib_ext=quotemeta($Config{lib_ext});
+my $libmarpa = '';
+find({wanted => \&wanted, no_chdir => 1}, $wanteddir);
+die "Cannot find libmarpa$Config{lib_ext}" if (! $libmarpa);
+
+my $copylibmarpa = File::Spec->catfile($olddir, basename($libmarpa));
+print "\n";
+print "*** Moving $libmarpa to $copylibmarpa\n";
+print "\n";
+move($libmarpa, $copylibmarpa) || die "Cannot move $libmarpa to $copylibmarpa, $!";
+
+my $copydir = File::Spec->catfile($olddir, 'libmarpa_build');   # Pretend destination is a file instead of a dir, to avoir eventual dir suffix
+print "\n";
+print "*** Moving $wanteddir to $copydir\n";
+print "\n";
+move($wanteddir, $copydir) || die "Cannot move $wanteddir to $copydir, $!";
 
 exit(EXIT_SUCCESS);
+
+sub wanted {
+    if (-f $_ && $_ =~ /$quotemetalib_ext$/) {
+	$libmarpa = $_;
+    }
+}
