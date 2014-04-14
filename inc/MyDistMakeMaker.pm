775a53c43e4fec5dd64f74f12a4220407a590333
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-04-14 07:55:36 +0200

    inc/MyDistMakeMaker

---
+++ inc/MyDistMakeMaker.pm
@@ -0,0 +1,38 @@
+package inc::MyDistMakeMaker;
+use Moose;
+use Config;
+use Marpa::R2 2.084000;   # So that we are sure Marpa could build here
+
+
+extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
+
+override _build_WriteMakefile_args => sub {
+    +{
+	# Add MYEXTLIB => to WriteMakefile() args
+	%{ super() },
+	MYEXTLIB => 'libmarpa' . $Config{lib_ext},
+    }
+};
+
+override _build_MakeFile_PL_template => sub {
+    my ($self) = @_;
+    my $template = super();
+ 
+    $template .= <<'TEMPLATE';
+package MY;
+use Config;
+
+sub postamble {
+    my $self = shift;
+    my @ret = (
+	$self->SUPER::postamble,
+	'libmarpa' . $Config{lib_ext} . ' :',
+	"\t" . '$(PERLRUN) BuildMarpa.pl Marpa-R2-2.084000.tar.gz',
+	''
+	);
+    return join "\n", @ret;
+}
+TEMPLATE
+ 
+    return $template;
+};
